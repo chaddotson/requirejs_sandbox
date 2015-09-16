3eda319148a5f49cc2e25ca20c1cf04ec1e77f2b
Chad Dotson <chad@cdotson.com>
2015-09-16 00:27:42 -0500

    initial commit.  Some copied from https://github.com/volojs/create-template

---
+++ www/app/main.js
@@ -0,0 +1,16 @@
+define(function (require) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    var sample = require('./sample');
+
+    // Load library/vendor modules using
+    // full IDs, like:
+    var print = require('print');
+
+    print(sample.getHello());
+});
+
+
+
+
